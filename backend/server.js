2a009aac86895baeeef5a7fdd2cc90ae81bd9412
Kaitlin Diaz <k.diaz1452@gmail.com>
2024-05-31 20:39:17 -0400

    Initial commit with frontend and backend setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) throw err;
+  const db = client.db('fletch');
+  console.log('Connected to MongoDB');
+  client.close();
+});
